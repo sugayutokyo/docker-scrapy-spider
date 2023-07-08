ab0b7416da623ab291f7b8778b6c2ee994c90b59
sugayutokyo <mygit_yutasugao20171126@yahoo.co.jp>
2023-07-08 15:21:09 +0900

    update: scrapy crawl getHtml

--- app/scraping/spiders/getHtml.py
+++ app/scraping/spiders/getHtml.py
@@ -1,10 +1,13 @@
 import scrapy
-
+import os
 
 class GethtmlSpider(scrapy.Spider):
     name = "getHtml"
     allowed_domains = ["zenn.dev"]
-    start_urls = ["https://zenn.dev"]
-
+    start_urls = ["https://zenn.dev/elletech/articles/nextjs_microcms"]
+    
     def parse(self, response):
-        pass
+        output_path = '/usr/src/app/downloads/'
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        with open(output_path + 'index.html', 'w', encoding='utf-8') as f:
+            f.write(response.text)
